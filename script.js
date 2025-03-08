a4fa4531de4b3e786f4c885e87925a095aac9af9
TawanaXploit <141659359+tawanamohammadi@users.noreply.github.com>
2025-03-08 04:21:40 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Particle.js Configuration
+particlesJS('particles-js', {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: '#00ff9d'
+        },
+        shape: {
+            type: 'circle'
+        },
+        opacity: {
+            value: 0.5,
+            random: true
+        },
+        size: {
+            value: 3,
+            random: true
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: '#00ff9d',
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 
